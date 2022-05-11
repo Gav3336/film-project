9914884d412869bb99ba74935ce5d08e7375a5dc
Gav3336 <74173063+Gav3336@users.noreply.github.com>
2022-05-11 19:48:26 +0200

    "aggiunto inizio top250"

--- src/app/comune/film-top.ts
+++ src/app/comune/film-top.ts
@@ -1,2 +1,10 @@
 export class FilmTop {
+    title: String = "";
+    image: String = "";
+    id: String = "";
+    year: String = "";
+    rating: String = "";
+    crew: String = "";
+  
+    constructor() {}
 }
--- src/app/top250/top250.component.ts
+++ src/app/top250/top250.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FilmTop } from '../comune/film-top';
 
 @Component({
   selector: 'app-top250',
@@ -6,10 +8,57 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./top250.component.css']
 })
 export class Top250Component implements OnInit {
+  url;
+  urlWiki;
 
-  constructor() { }
+  mostra: boolean;
+
+  vettoriDati: any;
+  vettoriWiki: any;
+
+  FilmCollector: FilmTop[] = [];
+
+  nomeFilm: String = "";
+
+  infoFilm: boolean = false;
+  FilmID: String = "";
+  logged: boolean = true;
+
+  constructor(private miohttp: HttpClient) {
+    this.url = "";
+    this.urlWiki = "";
+    this.mostra = false;
+   }
 
   ngOnInit(): void {
+    this.mostra = false;
+    this.chiamaDatiFilm();
+  }
+
+  chiamaDatiFilm() {
+    this.url = 'https://imdb-api.com/it/API/Top250Movies/k_hp5e67w2/' + this.nomeFilm;
+    this.mostra = false;
+    this.miohttp.get(this.url).subscribe((dati) => {
+      console.log(dati);
+      this.vettoriDati = dati;
+      this.mostra = true;
+
+      this.fillCollector();
+      console.log(this.FilmCollector)
+      
+    });
+  }
+
+  fillCollector() {
+    for (let i = 0; i < this.vettoriDati.items.lenght; i++) {
+      this.FilmCollector.push(new FilmTop());
+      this.FilmCollector[i].title = this.vettoriDati.items[i].title;
+      this.FilmCollector[i].image = this.vettoriDati.items[i].image;
+      this.FilmCollector[i].id = this.vettoriDati.items[i].id;
+      this.FilmCollector[i].year = this.vettoriDati.items[i].year;
+      this.FilmCollector[i].rating = this.vettoriDati.items[i].imDbRating;
+      this.FilmCollector[i].crew = this.vettoriDati.items[i].crew;
+    }
   }
 
 }
